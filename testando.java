af9fd62844b5f4cb499525192ecc36f35395d957
PauloAndre05 <91162058+PauloAndre05@users.noreply.github.com>
2021-11-29 22:29:54 +0000

    Add files via upload

---
+++ testando.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class testando{
+
+public static void main(String[] args){
+
+int a, b, p = 0;
+Scanner ler = new Scanner (System.in);
+System.out.println("digite um número: ");
+a = ler.nextInt();
+b = ler.nextInt();
+
+p = a + b;
+
+System.out.println("A soma dos dois numeros eh = "+p);
+}
+
+}
